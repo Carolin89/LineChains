20d6b2ea1a8fc302ae795c345d3d85b592fa9ba9
carolin <carolin.preuss89@web.de>
2024-08-27 15:37:59 +0200

    Simplify line chain length calculation

--- src/LineChain.java
+++ src/LineChain.java
@@ -2,13 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 class LineChain {
-    List<Line> lines = new ArrayList<>();
+    List<Line> lines;
+    double length;
+
+    public LineChain() {
+        this.lines = new ArrayList<>();
+        this.length = 0;
+    }
 
     public void addLine(Line line) {
         lines.add(line);
+        length += line.length();
+    }
+
+    List<Line> getLines(){
+        return lines;
     }
 
     public double getTotalLength() {
-        return lines.stream().mapToDouble(Line::length).sum();
+        return this.length;
     }
 }
